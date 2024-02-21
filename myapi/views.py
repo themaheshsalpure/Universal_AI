494510882b6c5268a7f7da7734884596d69f68b5
Mahesh Salpure <maheshsalpure171@gmail.com>
2024-02-21 23:28:50 +0530

    update

--- myapi/views.py
+++ myapi/views.py
@@ -3,8 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 import torch
-import json
-import os
+# import json
 from huggingface_hub import login
 
 huggingface_token = "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"
